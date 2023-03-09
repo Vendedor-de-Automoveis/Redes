49612f4c0b7e8034097ec57d13046f5a188a2cc8
Heitor Ribeiro Bernardes <heitor220052@ilum.cnpem.br>
2023-03-09 17:35:40 -0300

    aula 09 de março

---
+++ AlgoritmosGeneticos/01 - Algoritmos geneticos/funcoes.py
@@ -0,0 +1,10 @@
+def func_obj(indv):
+    ''' computa uma métrica do objetivo do problema
+    
+    args:
+    indv: uma lista que contém os genes, indivíduo
+    
+    return:
+        soma dos genes dos indivíduos
+    '''
+    return sum(indv) 
